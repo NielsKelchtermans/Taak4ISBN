e5cdaaad2b805b4178d1ac9abc0c8693fd297ea8
Niels Kelchtermans <kelchtermansniels@gmail.com>
2020-01-16 17:26:51 +0100

    Aanpassingen class ISBNTest

--- ISBN.cs
+++ ISBN.cs
@@ -6,20 +6,40 @@ namespace Taak4ISBN
 {
     public class ISBN
     {
-		//property Isbn
-		private string iSBNummerValue;
+        //property Isbn
+        private string iSBNummerValue;
 
-		public string ISBNummer
-		{
-			get { throw new NotImplementedException(); }
-			set { throw new NotImplementedException(); }
-		}
+        public string ISBNummer
+        {
+            get { return iSBNummerValue; }
+            set
+            {
+                throw new NotImplementedException(); 
+                /*if (value == null)
+                {
+                    throw new ArgumentNullException();
+                }
+                if (value.Length != 13)
+                {
+                    throw new ArgumentException();
+                }
+                for (int i = 1; i < value.Length; i++)
+                {
+                    var totaalOneven = int.Parse(value[0].ToString());
+                    var totaalEven = 0;
+                    if (i%2==0)
+                    {
+                        totaalOneven += int.Parse(value[i].ToString());
+                    }
+                }*/
+            }
+        }
 
-		//override method
-		public override string ToString()
-		{
-			throw new NotImplementedException();
-		}
+        //override method
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
 
-	}
+    }
 }
